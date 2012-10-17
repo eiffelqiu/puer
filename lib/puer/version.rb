23415aca5052510941ae2c75c50fda1a3b02c898
Eiffel Qiu <eiffelqiu@gmail.com>
2012-10-17 12:43:53 +0800

    fix version

--- lib/puer/version.rb
+++ lib/puer/version.rb
@@ -2,9 +2,8 @@ module Puer
   module Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 0
-    BUILD = '5'
+    PATCH = 5
     
-    STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.').chomp('.')
+    STRING = [MAJOR, MINOR, PATCH].compact.join('.').chomp('.')
   end
 end
